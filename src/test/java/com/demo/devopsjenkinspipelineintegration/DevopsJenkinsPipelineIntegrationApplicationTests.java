fde96a3c4072a0ed0e947f8b213f71b2d39a5de3
Jalandhar <gangajalandharreddy525@gmail.com>
2024-04-30 22:31:33 +0530

    First Commit

---
+++ src/test/java/com/demo/devopsjenkinspipelineintegration/DevopsJenkinsPipelineIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.devopsjenkinspipelineintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsJenkinsPipelineIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
